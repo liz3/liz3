374fd98e79c5c5167604b5bb847d68a969ddff69
Liz3 <accs@21xayah.com>
2020-03-06 21:21:31 +0100

    Added indent for js2-mode

--- init.el
+++ init.el
@@ -16,6 +16,7 @@ There are two things you can do about this warning:
   )
 (package-initialize)
 
+
 (setq-default tab-width 2
               indent-tabs-mode nil
               column-number-mode t
@@ -75,6 +76,7 @@ There are two things you can do about this warning:
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(js-indent-level 2)
  '(package-selected-packages
    (quote
     (magit kotlin-mode go-mode ## rjsx-mode js2-mode nord-theme seoul256-theme))))
