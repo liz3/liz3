eb52c9f4d13422fb2ed08e2245f25779b25eea69
Liz3 <accs@liz3.net>
2020-12-28 04:07:20 +0100

    updated the files

--- configs/init.el
+++ configs/init.el
@@ -85,83 +85,6 @@ There are two things you can do about this warning:
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 (show-paren-mode t)
 
-(defun xah-save-all-unsaved ()
-  "Save all unsaved files. no ask.
-Version 2019-11-05"
-  (interactive)
-  (save-some-buffers t ))
-
-;; when switching out of emacs, all unsaved files will be saved
-(add-hook 'focus-out-hook 'xah-save-all-unsaved)
-
-(setq auto-save-default nil)
-(setq create-lockfiles nil)
-
-(defun fira-code-mode--make-alist (list)
-  "Generate prettify-symbols alist from LIST."
-  (let ((idx -1))
-    (mapcar
-     (lambda (s)
-       (setq idx (1+ idx))
-       (let* ((code (+ #Xe100 idx))
-          (width (string-width s))
-          (prefix ())
-          (suffix '(?\s (Br . Br)))
-          (n 1))
-     (while (< n width)
-       (setq prefix (append prefix '(?\s (Br . Bl))))
-       (setq n (1+ n)))
-     (cons s (append prefix suffix (list (decode-char 'ucs code))))))
-     list)))
-
-(defconst fira-code-mode--ligatures
-  '("www" "**" "***" "**/" "*>" "*/" "\\\\" "\\\\\\"
-    "{-" "[]" "::" ":::" ":=" "!!" "!=" "!==" "-}"
-    "--" "---" "-->" "->" "->>" "-<" "-<<" "-~"
-    "#{" "#[" "##" "###" "####" "#(" "#?" "#_" "#_("
-    ".-" ".=" ".." "..<" "..." "?=" "??" ";;" "/*"
-    "/**" "/=" "/==" "/>" "//" "///" "&&" "||" "||="
-    "|=" "|>" "^=" "$>" "++" "+++" "+>" "=:=" "=="
-    "===" "==>" "=>" "=>>" "<=" "=<<" "=/=" ">-" ">="
-    ">=>" ">>" ">>-" ">>=" ">>>" "<*" "<*>" "<|" "<|>"
-    "<$" "<$>" "<!--" "<-" "<--" "<->" "<+" "<+>" "<="
-    "<==" "<=>" "<=<" "<>" "<<" "<<-" "<<=" "<<<" "<~"
-    "<~~" "</" "</>" "~@" "~-" "~=" "~>" "~~" "~~>" "%%"
-    "x" ":" "+" "+" "*"))
-
-(defvar fira-code-mode--old-prettify-alist)
-
-(defun fira-code-mode--enable ()
-  "Enable Fira Code ligatures in current buffer."
-  (setq-local fira-code-mode--old-prettify-alist prettify-symbols-alist)
-  (setq-local prettify-symbols-alist (append (fira-code-mode--make-alist fira-code-mode--ligatures) fira-code-mode--old-prettify-alist))
-  (prettify-symbols-mode t))
-
-(defun fira-code-mode--disable ()
-  "Disable Fira Code ligatures in current buffer."
-  (setq-local prettify-symbols-alist fira-code-mode--old-prettify-alist)
-  (prettify-symbols-mode -1))
-
-(define-minor-mode fira-code-mode
-  "Fira Code ligatures minor mode"
-  :lighter " Fira Code"
-  (setq-local prettify-symbols-unprettify-at-point 'right-edge)
-  (if fira-code-mode
-      (fira-code-mode--enable)
-    (fira-code-mode--disable)))
-
-(defun fira-code-mode--setup ()
-  "Setup Fira Code Symbols"
-  (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol"))
-(provide 'fira-code-mode)
-
-(define-globalized-minor-mode my-global-fc-mode fira-code-mode
-  (lambda () (fira-code-mode 1)))
-
-
-;;(setq x-super-keysym 'meta)
-(my-global-fc-mode 1)
-
 ;; Mac clipboard
 (defun copy-from-osx ()
   (shell-command-to-string "pbpaste"))
@@ -204,8 +127,8 @@ Version 2019-11-05"
 
 (global-set-key (kbd "ESC <up>") 'move-line-up)
 (global-set-key (kbd "ESC <down>") 'move-line-down)
-
 (global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-c l") 'magit-log-buffer-file)
 (global-set-key (kbd "C-x /") 'comment-dwim)
 
 (defun visit-inbox ()
--- liz3-site/index.md
+++ liz3-site/index.md
@@ -4,6 +4,7 @@ title: Home
 sidebar_sort_order: 1
 sidebar_link: true
 pfp: assets/img/pfp.png
+car: assets/img/car.jpg
 ---
 # Who Am i?
 Hey, i am Yann, (20) born in May 2000 from Germany.
@@ -16,10 +17,14 @@ Already in primary school i discovered my passion for computer science, i starte
 
 Usually i spent my nights in front of my laptop debugging something which doesn't work because i used a property wrong, but from time to time i do other things.
 
-I finished school in 2016, have been working at 2 companies so far, work on several smaller commissions and a own company i co own. For more see my [Projects](/projects).
+I finished school in 2016, have been working at 2 companies so far, work on several smaller commissions and a own company i co own. For more see my [Projects](/projects.html).
 
 Also i started drawing some time ago, but i am still in the very first steps with that.<br/>
 Lastly i am a big fan of Motorsport and cars.
+
+I own a BMW F30 330ix
+
+<img src="{{ page.car }}" style="height: 370px;width: auto;" />
 
 # Anxiety what?
 Yeah so that's a thing...i am writing that here because it's actually the main reason i created this site.
@@ -37,4 +42,4 @@ I am by no means a professional photographer, but i gathered some experience alr
 
 Currently i own a Nikon D750 with a 50mm f1.8 fix and my baby the 70-200mm f2.8, after a longer break i am starting to do photography again as a balance to all the computer stuff.
 # FAQ
-[FAQ Here](/faq)
+[FAQ Here](/faq.html)
